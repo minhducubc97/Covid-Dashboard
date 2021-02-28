544c5ba890d372a51298b983b6935bb5073d2c48
Duc Nguyen <ngminhduc20111997@gmail.com>
2021-02-27 17:37:07 -0800

    Minor color adjustment

--- src/components/CovidMapLegend/Densities.js
+++ src/components/CovidMapLegend/Densities.js
@@ -10,9 +10,9 @@ class Density {
 const Densities = [
     new Density("> 4,000,000", "#b30f00", "white", (cases) => cases > 4_000_000),
     new Density("2,000,000 - 4,000,000", "#b36200", "white", (cases) => cases <= 4_000_000 && cases > 2_000_000),
-    new Density("1,000,000 - 2,000,000", "#b39200", "black", (cases) => cases <= 2_000_000 && cases > 1_000_000),
-    new Density("500,000 - 1,000,000", "#adb300", "black", (cases) => cases <= 1_000_000 && cases > 500_000),
-    new Density("0 - 500,000", "#27b300", "black", (cases) => cases <= 500_000)
+    new Density("1,000,000 - 2,000,000", "#b39200", "white", (cases) => cases <= 2_000_000 && cases > 1_000_000),
+    new Density("500,000 - 1,000,000", "#adb300", "white", (cases) => cases <= 1_000_000 && cases > 500_000),
+    new Density("0 - 500,000", "#27b300", "white", (cases) => cases <= 500_000)
 ];
 
 export default Densities;
